cdc4ca94dffbf78deb509d480da9ec4268f2da33
trogdoraoeu <craig.muth@gmail.com>
2008-09-18 01:50:25 -0400

    Projects: added files

---
+++ projects.rb
@@ -0,0 +1,29 @@
+require 'auto_menu'
+
+class Projects
+
+  @@projects ||= {'default'=>'$tr'}
+
+  def self.menu
+#return [:hey]
+    [@@projects.map {|k,v| "#{k}: #{v}/"}, '.edit/'].flatten
+  end
+
+  def self.edit
+    puts "
+      - To edit the listing of projects, add a line like this:
+        ~/.el4r/
+          init.rb
+            | Projects.listProjects.listing = {:blog => '$tr', :foo => '/projects/foo'}
+        - The strings correspond to paths or bookmarks
+      "
+  end
+
+  def self.listing= the_listing
+    @@projects = the_listing
+  end
+
+  def self.listing
+    @@projects
+  end
+end
