0792cd35afd9005dc26555d2fdf452d34908f40d
trogdoraoeu <craig.muth@gmail.com>
2008-10-16 18:08:59 -0400

    CouchDb: .docs and .delete.

--- couch_db.rb
+++ couch_db.rb
@@ -5,7 +5,7 @@ class CouchDb
     if name.nil?   # If no name passed
       return (["- .start\n"] +
         (View.buffer_open?("*couchdb") ? ["  - running: **couchdb\n"] : []) +
-        ["- .admin_url\n", "+ .rest/\n"]).join('')
+        ["- .admin_url\n", "+ .databases/\n"]).join('')
     end
 
   end
@@ -19,7 +19,7 @@ class CouchDb
     nil
   end
 
-  def self.rest db=nil
+  def self.databases db=nil
 
     if db.nil?   # If no db yet, list all
       txt = Net::HTTP.get(URI.parse("#{@@server}/_all_dbs"))
@@ -27,6 +27,8 @@ class CouchDb
     end
 
     %Q[
+    - .docs/
+    - .delete/
     - GET #{@@server}/#{db}
       + create db: PUT
       + delete db: DELETE
@@ -39,6 +41,75 @@ class CouchDb
       - _design%2Fd1/
         + PUT {"views": {"v1": {"map": "function(doc){ emit(\\\"a\\\", null); }" }}}
     ]
+  end
+
+  def self.delete db, id=nil
+
+    # If no id, show all id's
+    if id.nil?
+      all = RestTree.request 'GET', "#{@@server}/#{db}_all_docs", nil
+      rows = JSON[all]['rows']
+      return rows.map{|i| "#{i['id']}/"}
+    end
+
+    self.escape_slashes id
+
+    # If id, look it up to get rev
+    record = RestTree.request 'GET', "#{@@server}/#{db}#{id}", nil
+    rev = JSON[record]['_rev']
+    # Delete it
+    RestTree.request 'DELETE', "#{@@server}/#{db}#{id}?rev=#{rev}", nil
+  end
+
+  def self.escape_slashes id
+    # If id has multiple slashes, escape all but the last
+    if id =~ /\/.+\/$/
+      id.sub! /\/$/, ''   # Remove last slash
+      id.gsub!('/', '%2F')   # Escape slashes
+      id.sub! /$/, '/'   # Put last back
+    end
+  end
+
+  def self.docs db, id=nil
+
+    # If no id, show all id's
+    if id.nil?
+      all = RestTree.request 'GET', "#{@@server}/#{db}_all_docs", nil
+      rows = JSON[all]['rows']
+      return rows.map{|i| "#{i['id']}/"}
+    end
+
+    self.escape_slashes id
+    children = CodeTree.children
+
+    # If id and no children, output doc
+    if ! children
+      record = RestTree.request 'GET', "#{@@server}/#{db}#{id}", nil
+
+      return record.gsub("\\n", "\n")
+    end
+
+    # If id and children, save children
+    newer = children.join("\n").unindent
+
+    # Get rev
+    record = RestTree.request 'GET', "#{@@server}/#{db}#{id}", nil
+
+    # If a record was found, add rev
+    if record !~ /404 /
+      rev = JSON[record]['_rev']
+
+      # Insert rev after first {, or replace if there already
+      if newer =~ /"_rev":"\d+"/
+        newer.sub! /("_rev":")\d+(")/, "\\1#{rev}\\2"
+      else
+        newer.sub! /\{/, "{\"_rev\":\"#{rev}\", "
+      end
+    end
+
+    # Update it
+    res = RestTree.request 'PUT', "#{@@server}/#{db}#{id}", newer
+    "|#{res}"
   end
 
 end
