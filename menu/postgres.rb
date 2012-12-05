2a3f1dafef4693c852d9d38843f31477390f2f40
trogdoro <craig.muth@gmail.com>
2012-12-04 21:57:16 -0800

    postgres: made it work.

--- menu/postgres.rb
+++ menu/postgres.rb
@@ -1,21 +1,43 @@
 class Postgres
 
-  def self.name= name
-    @@name = name
-  end
+  def self.menu db=nil, table=nil
 
-  def self.menu name=nil
+    # If just /, show all db's...
 
-    # If no name, list all names
-    if name.nil?
-      return Console.run("psql -c '\\l'", :sync=>true).scan(/^ (\w+)/).map{|i| "- #{i}/"}
+    if db.nil?
+      txt = Console.run("psql -c '\\l'", :sync=>true)
+      txt = txt.scan(/^ (\w+)/).map{|i| "- #{i[0]}/"}
+      return txt.join "\n"
     end
 
-    puts "
-      - .tables/
-      - .create_db :foo
-      - .drop_db :foo
-      "
+    # If /mydb/, show tables...
+
+    if table.nil?
+
+      txt = Console.run("psql #{db} -c '\\d'", :sync=>true)
+      txt = txt.split "\n"
+      txt = txt[3..-2]
+      output = ""
+      txt.each{|o|
+        columns = o.split(/ *\| */)
+        next if columns[2] != "table"
+        output << "- #{columns[1]}/\n"
+      }
+
+      return output
+    end
+
+    # If /mydb/mytable, show rows...
+
+    txt = Console.run("psql #{db} -c 'select * from #{table}'", :sync=>true)
+    txt = txt.split "\n"
+    header = txt.shift
+    txt.shift
+    txt.pop
+    txt.unshift header.strip
+    txt.map!{|o| "|#{o}"}
+    txt.join("\n")
+
   end
 
   def self.create_db name
