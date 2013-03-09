d750004dd60afeab83fed4d8fbca1199d1fadd18
Wilker Lucio <wilkerlucio@gmail.com>
2013-03-09 20:28:51 -0300

    refactored specs from code tree
    this refactoring improves the specs in order to
    have only one expectation per spec, conforming
    with spec best practices

--- spec/code_tree_spec.rb
+++ spec/code_tree_spec.rb
@@ -4,28 +4,62 @@ require 'xiki/code_tree'
 # require 'core_ext'
 
 describe CodeTree, "#extract_class" do
+  context "extracing methods" do
+    it "should extract starting with . and ending with /" do
+      CodeTree.extract_method("- .you/").should == "you"
+    end
 
-  it "should extract method" do
-    CodeTree.extract_method("- .you/").should == "you"
-    CodeTree.extract_method("- Hey.you").should == "you"
-    CodeTree.extract_method("- .you").should == "you"
-    CodeTree.extract_method("- Wiki.menu('hey')").should == "menu('hey')"
-    CodeTree.extract_method("- .pages('new')").should == "pages('new')"
-    CodeTree.extract_method("- Wiki1.menu('hey')").should == "menu('hey')"
-    CodeTree.extract_method("- .merb_local").should == "merb_local"
-    CodeTree.extract_method("- Remote.dir 'f.com:21'").should == "dir 'f.com:21'"
+    it "should extract when beginning with class" do
+      CodeTree.extract_method("- Hey.you").should == "you"
+    end
 
+    it "should extract starting with . with clean ending" do
+      CodeTree.extract_method("- .you").should == "you"
+    end
+
+    it "should extract with class and params" do
+      CodeTree.extract_method("- Wiki.menu('hey')").should == "menu('hey')"
+    end
+
+    it "should extract without class and having params" do
+      CodeTree.extract_method("- .pages('new')").should == "pages('new')"
+    end
+
+    it "should extract with class and params, having numbers into class" do
+      CodeTree.extract_method("- Wiki1.menu('hey')").should == "menu('hey')"
+    end
+
+    it "should extract method having underscore" do
+      CodeTree.extract_method("- .merb_local").should == "merb_local"
+    end
+
+    it "should extract method with params (without parentesis)" do
+      CodeTree.extract_method("- Remote.dir 'f.com:21'").should == "dir 'f.com:21'"
+    end
+
+    context "with junk at beginning" do
+      it "should extract rejecting the junk on beginning" do
+        CodeTree.extract_method("you there/").should == nil
+      end
+
+      it "should extract rejecting the beginning junk (even it looking like a method)" do
+        CodeTree.extract_method(".you 'Bla.there'/").should == "you 'Bla.there'"
+      end
+    end
   end
 
-  it "should extract method with junk at beginning" do
-    CodeTree.extract_method("you there/").should == nil
-    CodeTree.extract_method(".you 'Bla.there'/").should == "you 'Bla.there'"
-  end
+  context "extract class" do
+    it "should extract class name all lowercased" do
+      CodeTree.extract_class("wiki1.menu('hey')").should == nil
+    end
 
-  it "should extract class" do
-    CodeTree.extract_class("wiki1.menu('hey')").should == nil
-    CodeTree.extract_class("Wiki1.menu('hey')").should == "Wiki1"
-    CodeTree.extract_class("blaa Wiki1.menu('hey')").should == nil
+    it "should extract class with first letter uppercase" do
+      CodeTree.extract_class("Wiki1.menu('hey')").should == "Wiki1"
+    end
+
+    it "should extract class with junk at beginning" do
+      CodeTree.extract_class("blaa Wiki1.menu('hey')").should == nil
+    end
   end
 
   it "should get class from parent" do
