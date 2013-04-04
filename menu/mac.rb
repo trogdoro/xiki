39e59283ad8e64e6dff51a90123f84dac69da184
trogdoro <craig.muth@gmail.com>
2013-04-04 02:16:37 -0700

    Mac key: separated out.

--- menu/mac.rb
+++ menu/mac.rb
@@ -23,46 +23,37 @@ class Mac
     View.flash "- defined Command-C, Command-V, Command-Q, etc."
   end
 
-  #
   # Use this if you're using aquamacs
   #
   # Put this line in your init.rb file to use the below key mappings.
   #
   #   Mac.keys_for_aquamacs
-  #
-  def self.keys_for_aquamacs
+  def self.keys_for_aquamacs # options={}
 
     return if ! $el.boundp(:osx_key_mode_map)
 
+    # Don't do the rest by default, since it presumes that "Options > Appearance > Auto Faces" is off ...
+
     $el.define_key(:osx_key_mode_map, $el.kbd("A-0")) { Styles.font_size 110 }
     $el.define_key(:osx_key_mode_map, $el.kbd("A-=")) { Styles.zoom }
-    $el.define_key(:osx_key_mode_map, $el.kbd("A--")) { Styles.zoom :out=>1 }
+    $el.define_key(:osx_key_mode_map, $el.kbd("A--")) { Styles.zoom :out=>1}
+    $el.define_key(:osx_key_mode_map, $el.kbd("A-+")) { Styles.zoom :up=>1 }
+    $el.define_key(:osx_key_mode_map, $el.kbd("A-_")) { Styles.zoom :out=>1, :up=>1 }
 
     # Changes aquamacs behavior so it opens in same frame.  Is there
     # an aquamacs setting for this that could be used instead of redefining
     # key?
     $el.define_key :osx_key_mode_map, $el.kbd("A-n") do
-      View.to_buffer "untitled"
-      $el.rename_uniquely
-      Notes.mode
-      View.>> "\n"
+      View.new_file
     end
-
-    $el.define_key :osx_key_mode_map, $el.kbd("<A-return>") do
-      Launcher.launch_or_hide(:blink=>true)
-    end
-
-    $el.define_key :cua_global_keymap, $el.kbd("<C-return>"), nil   # Prohibit cua from monopolizing C-return key
 
   end
 
-  #
   # Use this if you're not using aquamacs
   #
   # Put this line in your init.rb file to use the below key mappings.
   #
-  #   Mac.keys
-  #
+  # Mac.keys
   def self.keys
 
     #   def self.keys
