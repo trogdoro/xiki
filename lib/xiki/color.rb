a308549971305a2a8ea06c58b21f98cfaf18b6d3
trogdoro <craig.muth@gmail.com>
2013-04-04 02:16:14 -0700

    Colors: added interactive menu and control-tabbing to step through them.

--- lib/xiki/color.rb
+++ lib/xiki/color.rb
@@ -5,18 +5,53 @@ require 'xiki/styles'
 class Color
 
   def self.menu
-    "
-    > Make current line be red, yellow, or blue
-    @Color.colorize :r
-    @Color.colorize :y
-    @Color.colorize :b
-
-    > See
-    << themes/
-    << styles/
-    "
+    %`
+    - .mark/
+      - .next/
+      - .previous/
+      - .outline/
+      - .show/
+      - light/
+      - red/
+      - orange/
+      - yellow/
+      - green/
+      - blue/
+      - purple/
+      - white/
+      - .delete/
+      - .clear/
+    - docs/
+      - overview/
+        | Temporarily marks lines as red or green etc. as a way to make
+        | them visually stand out.  This menu is primarily accessed via this
+        | key shortcut:
+        |
+        |   layout+mark
+        |
+        | (Control-l Control-m).  Try layout+mark+red on this line, for example.
+        | If you type it quickly the menu won't appear.
+      - keys/
+        @memorize/
+          | mark line light gray : layout+mark+light
+          | mark line green : layout+mark+green
+          | mark line red : layout+mark+red
+          | remove next color (except light) : layout+mark+delete
+          | remove all color (except light) : layout+mark+kill
+          | go to next color : layout+mark+next
+          | go to previous color : layout+mark+previous
+          | iterate through marked lines : 8+Tab
+    - api/
+      > Make current line be red, yellow, or blue
+      @Color.mark "red"
+      @Color.mark "yellow"
+      @Color.mark "green"
+    - see/
+      <@ themes/
+      <@ styles/
+      <@ effects/
+    `
   end
-
 
   @@colors = {
     "r"=>:color_rb_red, "y"=>:color_rb_yellow,
@@ -26,12 +61,24 @@ class Color
     "w"=>:color_rb_white,
   }
 
-  def self.colorize char=nil
-    char ||= Keys.input(:chars=>1, :prompt=>'Enter first letter of color: ')
-    char = char.to_s
-    # If h, just show all colors
-    case char
-    when "l"
+  @@colors_by_name = {
+    "light"=>:color_rb_light,
+    "red"=>:color_rb_red,
+    "orange"=>:color_rb_orange,
+    "yellow"=>:color_rb_yellow,
+    "green"=>:color_rb_green, "blue"=>:color_rb_blue, "purple"=>:color_rb_purple,
+    "white"=>:color_rb_white,
+  }
+
+  def self.mark color
+
+    # /mark/, so show options...
+
+    View.kill if View.name == "@color/mark/"
+
+    # Back in the original view...
+
+    if color == "light"
       # We want there to be only one "light" line per file, so delete existing
       overlays = $el.overlays_in(View.top, View.bottom)   # Get all overlays
       overlays.to_a.reverse.each do |o|   # Loop through and copy all
@@ -39,140 +86,88 @@ class Color
           $el.delete_overlay(o)
         end
       end
-
-    when "s"   # Search in all buffers for marked lines
-
-      prefix = Keys.prefix
-
-      if prefix.nil?
-        light = $el.overlays_in(View.top, View.bottom).to_a.find{|o| $el.overlay_get(o, :face).to_s == "color-rb-light"}
-        return View.to($el.overlay_start(light)) if light
-        # Else, contine on and do Color.search
-      end
-
-      return Launcher.open("- Color.search/") if Keys.prefix == 8
-
-      # Presumably :u
-      Launcher.open("- Color.search 'light'/")
-      return
-
-    when "o"   # Outline of marked lines
-      res = self.get_marked_lines
-      res.gsub! /^/, "    | "
-
-      file = View.file
-      path = file ?
-        "- #{File.expand_path(file).sub(/(.+)\//, "\\1/\n  - ")}\n" :
-        "- buffer #{View.name}/\n"
-
-      View.to_buffer("*outline of marked in #{path}")
-      View.clear;  Notes.mode
-      View.insert path
-      if res == "    | "
-        return View.insert "    - Nothing was marked in this file!"
-      end
-      View.insert res
-      Keys.clear_prefix
-      View.to_line 3
-      Line.to_beginning
-
-      Tree.search :left=> Line.left, :number_means_enter=>true
-
-      return
-
-    when "c"   # Copy marked lines
-
-      res = self.get_marked_lines
-
-      Clipboard['0'] = res
-
-    when "h"   # Hilight
-      prefix = Keys.prefix :clear=>1
-
-      ignore, left, right = View.txt_per_prefix prefix, :just_positions=>1, :default_is_line=>1
-
-      prefix == :u ?
-        Effects.glow(:color=>:rainbow, :times=>4) :
-        Effects.glow(:what=>[left, right], :times=>4)
-
-      #     when "h"   # Hide
-      #       Hide.hide_unless_block { |l, bol, eol|
-      #         # Whether current line contains an overlay of this color
-      #         $el.overlays_in(bol, eol).to_a.find{ |o|
-      #           $el.overlay_get(o, :face).to_s =~ /^color-rb-/
-      #         }
-      #       }
-      #       recenter(-3)
-      #       Hide.search
-      #       return
-    when "n"   # to next marker
-      Keys.prefix_times do
-        pos = $el.next_overlay_change(View.cursor)
-        pos = $el.next_overlay_change(pos) unless $el.overlays_at(pos)
-        View.to(pos)
-      end
-      return
-    when "p"   # to next marker
-      Keys.prefix_times do
-        pos = $el.previous_overlay_change(View.cursor)
-        pos = $el.previous_overlay_change(pos-2) if $el.overlays_at(pos-2)
-        View.to pos
-      end
-      return
-    when "d"
-      overlays = $el.overlays_at($el.next_overlay_change($el.point_at_bol - 1))
-      return View.beep "- No highlights after cursor!" if ! overlays
-      return $el.delete_overlay(overlays[0])
-    when "k"
-
-      if Keys.prefix_u   # Don't delete map mark
-        return $el.remove_overlays
-      end
-      overlays = $el.overlays_in(View.top, View.bottom)   # Get all overlays
-      overlays.to_a.reverse.each do |o|   # Loop through and copy all
-        if $el.overlay_get(o, :face).to_s != "color-rb-light"
-          $el.delete_overlay(o)
-        end
-      end
-      return
-
-    when "a"
-      return self.alternating
     end
 
-    case Keys.prefix
-    when :u   # If C-u, use region
-      left, right = View.range
-    when nil   # If nothing, do line
-      left, right = Line.left, Line.right+1
-    else   # Else, get N lines
-      txt, left, right = View.txt_per_prefix
-    end
 
-    if ! @@colors[char]
-      return View.message "No char color for '#{char}'"
-    end
-
+    left, right = Line.left, Line.right+1
     over = $el.make_overlay(left, right)
-    $el.overlay_put over, :face, @@colors[char]
+    $el.overlay_put over, :face, @@colors_by_name[color]
+
+    # Save time it was added
+    $el.overlay_put over, :created_at, Time.now.to_f.to_s
+
+    nil
   end
 
-  def self.alternating
-    orig = View.cursor
-    # Get region to cover
-    txt, left, right = View.txt_per_prefix
-    View.cursor = left
-    while(View.cursor < right)
-      Color.colorize_line :color_rb_light
-      Line.next 2
+  def self.next
+    View.kill if View.name == "@color/mark/"
+    pos = nil
+    Keys.prefix_times do
+      pos = $el.next_overlay_change(View.cursor)
+      pos = $el.next_overlay_change(pos) unless $el.overlays_at(pos)
+      View.to(pos)
     end
-    View.cursor = orig
+    pos != View.bottom
   end
 
-  def self.colorize_line face
-    over = $el.make_overlay(Line.left, Line.right+1)
-    $el.overlay_put over, :face, face
+  def self.previous
+    View.kill if View.name == "@color/mark/"
+    Keys.prefix_times do
+      pos = $el.previous_overlay_change(View.cursor)
+      pos = $el.previous_overlay_change(pos-2) if $el.overlays_at(pos-2)
+      View.to pos
+    end
   end
+
+  def self.clear_light
+
+    # We want there to be only one "light" line per file, so delete existing
+    overlays = $el.overlays_in(View.top, View.bottom)   # Get all overlays
+    overlays.to_a.reverse.each do |o|   # Loop through and copy all
+      if $el.overlay_get(o, :face).to_s == "color-rb-light"
+        $el.delete_overlay(o)
+      end
+    end
+
+  end
+
+  def self.delete
+    View.kill if View.name == "@color/mark/"
+
+    overlays = $el.overlays_at($el.next_overlay_change($el.point_at_bol - 1))
+    return View.beep "- No highlights after cursor!" if ! overlays
+    return $el.delete_overlay(overlays[0])
+  end
+
+  def self.clear
+    View.kill if View.name == "@color/mark/"
+
+    if Keys.prefix_u   # Don't delete map mark
+      return $el.remove_overlays
+    end
+
+    overlays = $el.overlays_in(View.top, View.bottom)   # Get all overlays
+    overlays.to_a.reverse.each do |o|   # Loop through and copy all
+      if $el.overlay_get(o, :face).to_s != "color-rb-light"
+        $el.delete_overlay(o)
+      end
+    end
+
+    nil
+  end
+
+
+  #   def self.alternating
+  #     orig = View.cursor
+  #     # Get region to cover
+  #     txt, left, right = View.txt_per_prefix
+  #     View.cursor = left
+  #     while(View.cursor < right)
+  #       Color.colorize_line :color_rb_light
+  #       Line.next 2
+  #     end
+  #     View.cursor = orig
+  #   end
 
   def self.define_styles   # For Keys.layout_kolor_light, etc.
 
@@ -215,44 +210,20 @@ class Color
   end
 
   def self.get_marked_lines label=nil
+Ol.stack 3
+
     overlays = $el.overlays_in(View.top, View.bottom)   # Get all overlays
-    res = ""
+    txt = ""
     overlays.to_a.reverse.each do |o|   # Loop through and copy all
       if label
         next if $el.overlay_get(o, :face).to_s != label
       end
-      line = View.txt($el.overlay_start(o), overlay_end(o))
+      line = View.txt($el.overlay_start(o), $el.overlay_end(o))
       line << "\n" unless line =~ /\n$/
-      res << line
+      txt << line
     end
-    res
-  end
-
-  def self.search label=nil
-    label = "color-rb-#{label}" if label
-
-    orig = View.buffer
-    txt = ""
-    Buffers.list.to_a.each do |b|  # Each buffer open
-
-      $el.set_buffer b
-      res = self.get_marked_lines label
-      next if res.empty?
-
-      file = View.file
-      next unless file
-      path = file ?
-        "@#{File.expand_path(file).sub(/(.+)\//, "\\1/\n  - ")}\n" :
-        "- buffer #{View.name}/\n"
-
-      txt << path
-      txt << res.gsub!(/^/, "    | ")
-
-    end
-    View.to_buffer orig
     txt
   end
-
 
   # Returns list of colors at cursor
   # Color.at_cursor
@@ -263,5 +234,101 @@ class Color
     end
   end
 
+  def self.outline
+    View.kill if View.name == "@color/mark/"
+
+    txt = self.get_marked_lines
+    if txt.blank?
+      txt = "    - no marked lines in this view!"
+    else
+      txt.gsub! /^/, "    | "
+    end
+
+    file = View.file
+
+    path = file ?
+      "- #{File.expand_path(file).sub(/(.+)\//, "\\1/\n  - ")}\n" :
+      "- buffer #{View.name}/\n"
+
+    txt = "#{path}#{txt}"
+
+    Launcher.open txt, :no_launch=>1
+
+    nil
+  end
+
+
+
+  # Builds up hash of all marks, sorted by time.
+  # Structure of hash returned:
+  # - key: time
+  # - value: [file, line, color]
+  def self.all_marks_hash
+    hash = {}
+    orig = View.buffer
+
+    # For each buffer, add marked lines to hash...
+
+    Buffers.list.to_a.each do |b|  # Each buffer open
+      $el.set_buffer b
+
+      file = View.file
+
+      next if ! file   # For now, don't try to handle buffers
+
+      # TODO: Rework to add to hash
+      overlays = $el.overlays_in(View.top, View.bottom)   # Get all overlays
+      overlays.to_a.each do |o|   # Loop through and copy all
+        line = View.txt($el.overlay_start(o), $el.overlay_end(o))
+        face = $el.overlay_get(o, :face)
+        created_at = $el.overlay_get(o, :created_at)
+        next unless created_at && line.any? && face.any?
+        hash[created_at] = [file, line, face.to_s]
+      end
+    end
+
+    View.to_buffer orig
+    hash
+  end
+
+  def self.show
+    hash = self.all_marks_hash
+
+    if hash.empty?   # If no marks found, just say so
+      return "- no marks found!"
+    end
+
+    keys = hash.keys.sort.reverse
+
+    txt = ""
+    last_file = nil
+    keys.each do |key|
+      file, line = hash[key]
+      if last_file == file   # If same file as last, just add line
+        txt << "    | #{line}"
+      else # Else, show file and line
+        txt << "@#{file.sub /(.+\/)/, "\\1\n  - "}\n    | #{line}"
+      end
+
+      last_file = file
+    end
+
+    Tree.<< txt, :no_search=>1
+
+    # Apply colors...
+
+    keys.each do |key|
+      Move.to_quote
+      over = $el.make_overlay(Line.left+Line.indent.length, Line.right+1)
+      $el.overlay_put over, :face, hash[key]
+    end
+
+    Tree.to_parent :u
+    Move.to_quote
+
+    nil
+  end
+
 end
+
 Color.define_styles
