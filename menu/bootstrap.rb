10c8126bc9c5844611958ba993af1245e524906c
trogdoro <craig.muth@gmail.com>
2013-05-15 20:29:20 -0700

    Bootstrap: updgraded to 2.3.1.

--- menu/bootstrap.rb
+++ menu/bootstrap.rb
@@ -1,8 +1,9 @@
 class Bootstrap
 
-  @@relative = ""
+  #   @@relative = ""
   #   @@relative = "http://xiki.loc"
-  @@assets = "#{@@relative}/assets"
+  #   @@assets = "#{@@relative}/assets"
+  #   @@assets = "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1"
   #   @@assets = "http://twitter.github.com/bootstrap/assets"
 
   def self.menu_after *args
@@ -453,6 +454,7 @@ class Bootstrap
 
     # Pull out certain tags, so they can be used elsewhere.
 
+    # Created by - navbar/ item if one was there
     @navbar = @html.slice!(/ *<navbar>\n(.+?) *<\/navbar>\n/m)
 
     @scripts = ""
@@ -499,22 +501,29 @@ class Bootstrap
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
           <!-- Le styles -->
-          <link href="#{@@assets}/css/bootstrap.css" rel="stylesheet">
-          <style>
+          <!-- <link href="../assets/css/bootstrap.css" rel="stylesheet"> -->
+          <style type="text/css">
             body {
-              padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+              padding-top: 60px;
               padding-bottom: 40px;
             }
           </style>
-          <link href="#{@@assets}/css/bootstrap-responsive.css" rel="stylesheet">
+          <!-- <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet"> -->
 
-          <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+         <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+
+          <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
           <!--[if lt IE 9]>
-            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+            <script src="../assets/js/html5shiv.js"></script>
           <![endif]-->
 
-          <!-- Le fav and touch icons -->
-          <link rel="shortcut icon" href="#{@@assets}/ico/favicon.ico">
+          <!-- Fav and touch icons
+          <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+          <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+            <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+                          <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+                                         <link rel="shortcut icon" href="../assets/ico/favicon.png">
+          -->
         </head>
 
         <body>
@@ -522,11 +531,44 @@ class Bootstrap
           <div class="navbar navbar-inverse navbar-fixed-top">
             <div class="navbar-inner">
               <div class="container">
-        #{navbar}
+                #{navbar}
+                <!-- old navbar stuff moved from here to ruby comments below -->
               </div>
             </div>
           </div>
       `.unindent
+
+    #                 <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+    #                   <span class="icon-bar"></span>
+    #                   <span class="icon-bar"></span>
+    #                   <span class="icon-bar"></span>
+    #                 </button>
+    #                 <a class="brand" href="#">Project name</a>
+    #                 <div class="nav-collapse collapse">
+    #                   <ul class="nav">
+    #                     <li class="active"><a href="#">Home</a></li>
+    #                     <li><a href="#about">About</a></li>
+    #                     <li><a href="#contact">Contact</a></li>
+    #                     <li class="dropdown">
+    #                       <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+    #                       <ul class="dropdown-menu">
+    #                         <li><a href="#">Action</a></li>
+    #                         <li><a href="#">Another action</a></li>
+    #                         <li><a href="#">Something else here</a></li>
+    #                         <li class="divider"></li>
+    #                         <li class="nav-header">Nav header</li>
+    #                         <li><a href="#">Separated link</a></li>
+    #                         <li><a href="#">One more separated link</a></li>
+    #                       </ul>
+    #                     </li>
+    #                   </ul>
+    #                   <form class="navbar-form pull-right">
+    #                     <input class="span2" type="text" placeholder="Email">
+    #                     <input class="span2" type="password" placeholder="Password">
+    #                     <button type="submit" class="btn">Sign in</button>
+    #                   </form>
+    #                 </div>
+
 
     if @html =~ /class='hero-unit'/
       bg = @@bg[rand @@bg.length]
