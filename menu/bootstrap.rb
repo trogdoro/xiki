b0c49fdb93ea3665c6eeb279af26e88076e2d140
trogdoro <craig.muth@gmail.com>
2013-05-17 00:25:47 -0700

    Bootstrap: fixed messed up jquery url.

--- menu/bootstrap.rb
+++ menu/bootstrap.rb
@@ -612,7 +612,7 @@ module Xiki
               <!-- Le javascript
               ================================================== -->
               <!-- Placed at the end of the document so the pages load faster -->
-              <script src="http:////ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+              <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
               <script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
 
           #{@scripts}
