5a846e5b121b6728121536c447defd7b877c4597
trogdoro <craig.muth@gmail.com>
2013-07-26 02:07:29 -0700

    applescript: got menu working.

--- lib/xiki/tools/applescript.rb
+++ lib/xiki/tools/applescript.rb
@@ -1,31 +1,6 @@
 module Xiki
   class Applescript
 
-    #   def self.menu *command
-    def self.menu
-      %`
-      > Pass in applescript
-      | tell application "iTunes" to playpause
-      - .api/
-        | Run applescript
-        @Applescript.run 'tell application "iTunes" to playpause'
-        |
-        | Shortcut to wrap "tell" in block
-        @Applescript.run "playpause", :app=>"iTunes"
-        @Applescript.run "get the name of every track of library playlist 1 as string", :app=>"iTunes", :delimiter=>"|"
-      `
-    end
-
-    def self.menu_after output, *args
-
-      return output if output   # If menu handled it, just return
-
-      command = ENV['txt']
-      result = self.run command
-
-      ".flash - ran it!"
-    end
-
     #
     # Applescript.run 'tell application "iTunes" to playpause'
     # Applescript.run "playpause", :app=>"iTunes"
