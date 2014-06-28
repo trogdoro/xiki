ab846bb60943be93ddf9796a0df54c50b42cc743
dongdiddler <dongdiddler@yandex.com>
2014-06-28 17:14:09 +0000

    changed ip to use Socket.ip_address_list instead of ifconfig

--- menu/ip.rb
+++ menu/ip.rb
@@ -1,6 +1,6 @@
 # return "127.3.3.7"   # Hard-coded IP, for when demoing
 
-inet = `ifconfig`.split("\n").grep(/\binet\b/)
+# https://stackoverflow.com/a/7809304
 
-return "| #{inet[0][/[\d.]+/]}" if inet.length < 2
-inet[1][/[\d.]+/]
+ip_addr = Socket.ip_address_list.detect { |ip| ip.ipv4_private? }
+return "| #{ip_addr.ip_address}"
