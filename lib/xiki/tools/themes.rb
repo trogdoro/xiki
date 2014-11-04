e72fbd27d3adbfff13a063b8882f43ace317f6a2
trogdoro <craig.muth@gmail.com>
2014-11-03 18:54:49 -0800

    New theme that looks like a tab at the bottom.

--- lib/xiki/tools/themes.rb
+++ lib/xiki/tools/themes.rb
@@ -3,20 +3,51 @@ module Xiki
   # Lists themes and run them. Make files here
   # to make more themes:
   #
-  # @$x/etc/themes/
+  # =:xiki/misc/themes/
   #
   class Themes
 
-    def self.use name
-      path = "#{Xiki.dir}etc/themes/#{name.gsub " ", "_"}.notes"
+    def self.use name # , options={}
+
+      path = "#{Xiki.dir}misc/themes/#{name.gsub " ", "_"}.notes"
       raise "- Theme doesn't exist!" if ! File.exists? path
 
       load path
 
-      # This should be done manually, since many themes can be used in a row
-      #       Styles.reload_styles
+      if ControlLock.enabled?
+        $el.control_lock_apply_bar_color
+      end
 
       nil
     end
+
+    def self.init_in_client
+
+      $el.el4r_lisp_eval %`
+        (defun xiki-bar-add-padding (txt &rest format-space)
+          (let ((total (window-width)) left right)
+            (setq left (- total (length txt)))
+            (setq left (/ left 2))
+            (when (> 0 left) (setq left 2))
+            (setq right (- total (+ left (length txt))))
+            (when (> 0 right) (setq right 2))
+            (if format-space
+              (concat
+                (propertize (make-string left ? ) 'face 'mode-line-sides)   ; Make string of spaces
+                txt
+                (propertize (make-string right ? ) 'face 'mode-line-sides)   ; Make string of spaces
+              )
+              (concat
+                (make-string left ? )   ; Make string of spaces
+                txt
+                (make-string right ? )   ; Make string of spaces
+              )
+            )
+          )
+        )
+      `
+
+    end
+
   end
 end
