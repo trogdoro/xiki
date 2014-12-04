e396d5e1c041814f90c808fab44bc8816f9b8625
trogdoro <craig.muth@gmail.com>
2014-12-03 20:32:45 -0800

    Echo and options commands > to help inspect command processing.

---
+++ commands/echo.rb
@@ -0,0 +1,31 @@
+ancestors = options[:ancestors]
+menu_steps = ancestors && ancestors[-1] == "menu steps/"
+ancestors.pop if menu_steps
+
+# / and no ancestors, so display message...
+
+if ! ancestors && args == []
+  return %`
+    : The 'echo' menu is just for experimenting with how
+    : paths work. Put some stuff underneath and expand them
+    : to see the path. You'll see the value that's passed
+    : into menus as the "path" variable.
+    - a/
+      - b/
+    :
+    <= options/
+    <= source/
+    `
+end
+
+# =echo/foo, so use foo as the path...
+
+path = args.any? ?
+  args.inspect :
+  options[:ancestors].inspect
+
+# ancestors/=echo, so use ancestors...
+
+path << "\n<= menu steps/" if ! menu_steps
+
+path
--- menu/echo.rb
+++
@@ -1,2 +0,0 @@
-class Xiki::Echo < Xiki::Args
-end
