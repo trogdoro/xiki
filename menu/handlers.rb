46e692314f4c248be6886d22832b83c9706a8fdd
trogdoro <craig.muth@gmail.com>
2014-12-05 20:35:41 -0800

    Handlers command > improved.

--- menu/handlers.rb
+++
@@ -1,7 +0,0 @@
-dir = Bookmarks["$x/lib/xiki/"]
-
-if args.blank?
-  Dir["#{dir}*_handler.rb"].map {|f| "- #{f[/(\w+)_handler/, 1]}/"}.join("\n")
-else
-  "@open file/#{dir}/#{args[0]}_handler.rb"
-end
