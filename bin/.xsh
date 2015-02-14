1dafa93789350cac6e387db35797d0158e1904bc
trogdoro <craig.muth@gmail.com>
2015-02-13 18:16:51 -0800

    Install > created external shell key shortcuts.

---
+++ bin/.xsh
@@ -0,0 +1,172 @@
+#!/bin/bash
+
+#
+# This file should be included from your bash or zsh config. It
+# defines key shortcuts (by default Ctrl+X and Ctrl+G) that let
+# you quickly go back and forth between xsh and your existing
+# shell. And it defines the 'xsh' command wrapper, which let's xsh
+# send commands back to your shell.
+#
+# Example:
+#
+#   ~/.bashrc
+#     : xiki_open_key="\C-x"   # Ctrl+X to open xsh
+#     : xiki_drop_key="\C-d"   # Ctrl+D to show dropdown
+#     : xiki_grab_key="\C-g"   # Ctrl+G to grab to and from xsh
+#     : source /Users/craig/Dropbox/xiki/bin/.xsh
+#
+
+# Wraps the 'xsh' shell command.  Calls it and then runs any commands
+# it wrote to ~/xiki/misc/tmp/grabbed_commands.notes.
+function xsh {
+
+  # Ctrl+D on blank line, so do normal shell behavion and exit (zsh gives us control when Ctrl+D on a blank line)...
+
+  if [ "$1" = "-d" ] && [ $# = 1 ]; then
+    echo "exit"
+    exit
+  fi
+
+  # Escape+Ctrl+R, so write history to a temp file
+
+  if [ "$1" = "-r" ]; then
+    dir="$HOME/xiki/misc/tmp"
+    mkdir -p $dir
+    history -w "$dir/reverse_history.notes"
+  fi
+
+  command xsh $*
+
+  # Grab any "go" text, and run it...
+
+  if [ -f "$HOME/xiki/misc/tmp/grabbed_commands.notes" ]; then
+
+    dir=`pwd`
+    if [[ ! $dir =~ ^[a-zA-Z/_.-]+$ ]]; then
+      dir="\"$dir\""
+    fi
+    cd_to_ignore="cd $dir"
+
+    commandz=""
+
+    # Run each line in the file
+    while read p; do
+
+      # Don't do redundant cd (to the same dir)
+      if [ "$p" != "$cd_to_ignore" ]; then
+
+        # Store in a var and eval later, since eval inside this
+        # loop doesn't allow async commands like top, etc.
+        commandz+=$p
+        commandz+=$'\n'
+
+      fi
+
+    done <$HOME/xiki/misc/tmp/grabbed_commands.notes
+
+    # Delete it when we're done
+    rm $HOME/xiki/misc/tmp/grabbed_commands.notes
+
+    SAVEIFS=$IFS   # Store original separator
+    IFS=$'\n' y=($commandz)
+
+    for i in "${y[@]}"; do
+
+      i="${i%"${i##*[![:space:]]}"}"   # remove trailing whitespace characters
+
+      if [ -n "$ZSH_VERSION" ]; then
+        print -s $i
+      else   # Assume bash or bash-compatible
+        history -s $i
+        # It seems to be replacing :(
+        # Todo > deal with this later
+        # - maybe write the current command too?
+        #   - how would we know that? > see what's in end of the history now?
+        #   - see how it works in newer bash first
+      fi
+
+      # Show the command
+      echo \$ $i
+      # Run the command, it's output will be shown
+      eval $i
+    done
+
+    IFS=$SAVEIFS   # Restore original separator
+
+  fi
+
+  # Todo > have key that echos the output of the command
+  #   - to > the external shell
+  #   - ideas > up+grab, as+output__, window+output__, jump+__
+  # echo -e "~/\n  - a/\n  - b/"
+
+}
+
+#
+# Define key shortcuts...
+#
+
+# 'xiki open' shortcut (usually Ctrl+X) var exists, so define key...
+
+if [ $xiki_open_key ]; then
+
+  # It's Ctrl+X in bash, so undefine it first...
+
+  if [ $BASH_VERSION ] && [ $xiki_open_key = "\C-x" ]; then
+    bind "'\C-x' end-of-line"   # Causes C-x to be bindable
+  fi
+
+  # Define it
+
+  if [ -n "$ZSH_VERSION" ]; then
+    bindkey -s $xiki_open_key '\C-axsh \n'
+  else   # Assume bash or bash-compatible
+
+    #echo "heyyy"
+    # bind '"'$xiki_open_key'" "\C-axsh \n"'
+    bind \"$xiki_open_key'" "\C-axsh \n"'
+
+  fi
+fi
+
+# 'xiki grab' shortcut (usually Ctrl+G) var exists, so define key...
+
+if [ $xiki_grab_key ]; then
+  if [ -n "$ZSH_VERSION" ]; then
+    bindkey -s $xiki_grab_key '\C-axsh -g \n'
+  else   # Assume bash or bash-compatible
+    bind \"$xiki_grab_key'" "\C-axsh -g \n"'
+  fi
+fi
+
+# 'xiki dropdown' shortcut (usually Ctrl+D) var exists, so define key...
+
+if [ $xiki_dropdown_key ]; then
+  if [ -n "$ZSH_VERSION" ]; then
+    bindkey -s $xiki_dropdown_key '\C-axsh -d \n'
+  else   # Assume bash or bash-compatible
+    bind \"$xiki_dropdown_key'" "\C-axsh -d \n"'
+  fi
+fi
+
+
+# TODO > probably remove this > Drop+Tab is probably sufficient
+# 'xiki tab' shortcut (usually Option+Tab) var exists, so define key...
+
+if [ $xiki_tab_key ]; then
+  if [ -n "$ZSH_VERSION" ]; then
+    bindkey -s $xiki_tab_key '\C-axsh -t \n'
+  else   # Assume bash or bash-compatible
+    bind \"$xiki_tab_key'" "\C-axsh -t \n"'
+  fi
+fi
+
+# 'xiki reverse' shortcut (usually Option+Ctrl+R) var exists, so define key...
+
+if [ $xiki_reverse_key ]; then
+  if [ -n "$ZSH_VERSION" ]; then
+    bindkey -s $xiki_reverse_key '\C-axsh -r \n'
+  else   # Assume bash or bash-compatible
+    bind \"$xiki_reverse_key'" "\C-axsh -r \n"'
+  fi
+fi
---
+++ commands/make_xsh_available.rb
@@ -0,0 +1,228 @@
+module Menu
+
+  class MakeXshAvailable
+
+    def self.bash_conf_file
+      ["~/.bash_profile", "~/.bash_login", "~/.profile"].find{|o| File.exists? File.expand_path(o)}# || "fu"
+    end
+
+    def self.zsh_conf_file
+      ["~/.zshrc"].find{|o| File.exists? File.expand_path(o)}
+    end
+
+
+    def self.files_to_update
+
+      # Start with files that exist (plus always .bashrc)...
+
+      files = [
+        "~/.bashrc",
+        self.bash_conf_file,
+        self.zsh_conf_file,
+      ].compact
+
+      # Remove ones that exist and contain "source ~/.xsh" already
+
+      files = files.select do |file|
+        txt = File.read(File.expand_path file) rescue nil
+        next true if ! txt   # Only .bashrc might not exist, and we still want to create it, so leave it in the list
+        next false if txt =~ /^source ~\/\.xsh\b/   # It's already there, so do nothing
+        true
+      end
+
+      files
+    end
+
+    def self.files_to_update_preview
+      files = self.files_to_update
+
+      txt = files.map do |file|
+        "
+        #{file}
+          :+source ~/.xsh
+        ".unindent.strip
+      end.join("\n")
+
+      # Add if > .xsh exists:
+      if File.exists? File.expand_path("~/.xsh")
+        txt << "\n"+%`
+          ~/.xsh
+            :?Will be updated with new configuration
+        `.unindent.strip
+      end
+
+      txt.strip
+    end
+
+    MENU = %`
+      | Explore these menu items to find the setup that's right
+      | for you. (Use the arrow keys and type Ctrl+X to expand
+      | and collapse each of them).
+      |
+      + the 'xsh' command, and remapped key shortcuts/
+        | Enable the 'xsh' shell command. Also remap the following
+        | key shortcuts in your existing shell, to make them quickly
+        | switch to xsh:
+        |
+        |   Ctrl+X  Expand a shell shell command in xsh
+        |   Ctrl+D  Dropdown of options for a shell command
+        |   Ctrl+G  Grab shell commands to and from Xiki
+        |   Escape+Tab     Auto-complete
+        |   Escape+Ctrl+R  Shell history
+        |
+        | This is recommended if you (and any other people who use this
+        | account) don't use Ctrl+X, Ctrl+D, and Ctrl+G in your shell.
+        |
+        + enable this configuration
+          | Expand 'save' below, and the following changes will be made:
+          |\n#{self.files_to_update_preview.gsub(/^/, '          ')}
+          |
+          + .save
+          |
+        |
+        | (You can use the arrow keys and Ctrl+X to select the above item.)
+        |
+      - xsh 'xsh' command, and safer key shortcuts/
+        | This is like the previous option, but creates key shortcuts
+        | that don't conflict with your shells default shortcuts:
+        |
+        |   Escape+Ctrl+X  Expand a shell shell command in xsh
+        |   Escape+Ctrl+D  Dropdown of options for a shell command
+        |   Escape+Ctrl+G  Grab shell commands to and from Xiki
+        |   Escape+Tab     Auto-complete
+        |   Escape+Ctrl+R  Shell history
+        |
+        + enable this configuration
+          | Expand 'save' below, and the following changes will be made:
+          |\n#{self.files_to_update_preview.gsub(/^/, '          ')}
+          |
+          + .save
+          |
+        |
+        | (You can use the arrow keys and Ctrl+X to select the above item.)
+        |
+      + just the 'xsh' command/
+        | Just install the 'xsh' command, and don't add any key
+        | shortcuts in your existing shell.
+        |
+        + enable this configuration
+          | Expand 'save' below, and the following changes will be made:
+          |\n#{self.files_to_update_preview.gsub(/^/, '          ')}
+          |
+          + .save
+          |
+        |
+    `
+
+    def self.save choice, confirmed=nil
+
+      options = yield
+      options[:no_slash] = 1
+
+      txt = %`
+        #!/bin/sh
+
+        # Make the 'xsh' command available in all shells
+        export PATH=$PATH:#{Xiki.dir}bin
+      `.unindent
+
+      txt << "\n"
+
+      case choice
+      when /remapped key shortcuts/
+        txt << %`
+          # Define some key shortcuts for quickly switching from your shell to xsh
+          xiki_open_key="\\C-x"        # Ctrl+X to expand in xsh
+          xiki_dropdown_key="\\C-d"    # Ctrl+D to show a dropdown menu for a command
+          xiki_grab_key="\\C-g"        # Ctrl+G to grab commands to and from xsh
+          xiki_tab_key="\\e\\C-i"       # Escape+Tab to do autocomplete
+          xiki_reverse_key="\\e\\C-r"   # Escape+Ctrl+R to search shell history
+
+          # Some examples, in case you want to update this file:
+          # xiki_open_key="\\C-x"      # Ctrl+X
+          # xiki_open_key="\\e\\C-X"    # Escape+Ctrl+X
+
+          # Enable the key shortcuts and the xsh wrapper function
+          source #{Xiki.dir}bin/.xsh
+        `.unindent
+      when /safer key shortcuts/
+        txt << %`
+          # Define some key shortcuts for quickly switching from your shell to xsh
+          xiki_open_key="\\e\\C-x"        # Escape+Ctrl+X to expand in xsh
+          xiki_dropdown_key="\\e\\C-d"    # Escape+Ctrl+D to show a dropdown menu for a command
+          xiki_grab_key="\\e\\C-g"        # Escape+Ctrl+G to grab commands to and from xsh
+          xiki_tab_key="\\e\\C-i"       # Escape+Tab to do autocomplete
+          xiki_reverse_key="\\e\\C-r"   # Escape+Ctrl+R to search shell history
+
+          # Some examples, in case you want to update this file:
+          # xiki_open_key="\\C-x"      # Ctrl+X
+          # xiki_open_key="\\e\\C-X"    # Escape+Ctrl+X
+
+          # Enable the key shortcuts and the xsh wrapper function
+          source #{Xiki.dir}bin/.xsh
+        `.unindent
+      else
+        txt << %`
+          # Commented out, since user chose no key shortcuts:
+          #
+          # Define some key shortcuts for quickly switching from your shell to xsh
+          # xiki_open_key="\\C-x"        # Ctrl+X to expand in xsh
+          # xiki_dropdown_key="\\C-d"    # Ctrl+D to show a dropdown menu for a command
+          # xiki_grab_key="\\C-g"        # Ctrl+G to grab commands to and from xsh
+          # xiki_tab_key="\\e\\C-i"       # Escape+Tab to do autocomplete
+          # xiki_reverse_key="\\e\\C-r"   # Escape+Ctrl+R to search shell history
+          #
+          # Some examples, in case you want to update this file:
+          # xiki_open_key="\\C-x"      # Ctrl+X
+          # xiki_open_key="\\e\\C-X"    # Escape+Ctrl+X
+          #
+          # Enable the key shortcuts and the xsh wrapper function
+          # source #{Xiki.dir}bin/.xsh
+        `.unindent
+      end
+
+      txt << "\n"
+
+      dot_xsh_path = File.expand_path("~/.xsh")
+
+      # Make backup if it exists
+      if File.exists? dot_xsh_path
+        FileUtils.cp dot_xsh_path, "#{dot_xsh_path}.backup.#{rand 9999}"
+      end
+
+      # Write the new file...
+
+      File.open(dot_xsh_path, "w") { |f| f << txt }
+
+      # Include it from the other files...
+
+
+      files = self.files_to_update
+
+      files.each do |file|
+        file = File.expand_path file
+        txt = File.read(file) rescue ""
+
+        File.open(file, "w") { |f| f << "#{txt.sub(/\n+\z/, '')}\n\n\nsource ~/.xsh\n\n" }
+      end
+
+      return "
+        =replace/siblings/3/
+          |+Success!
+          |
+          | Your shell conf files have been updated. You can now type
+          | 'xsh' from any directory.
+          |
+          | Note: be sure to close any existing shell sessions before
+          | trying it, since config updates won't be reflected in them.
+          |
+          | Xsh is alpha at this point. Experiment, but be careful!
+          |
+          | Expand this item for help with using Xiki:
+          |
+          << help
+        ".unindent
+
+    end
+  end
+end
