6530379d3159d9f9180707eb01c30ea0d2e1488a
trogdoro <craig.muth@gmail.com>
2015-02-13 18:16:14 -0800

    Elisp command > inspect by default.

--- commands/elisp.rb
+++ commands/elisp.rb
@@ -7,8 +7,12 @@ return "
   " if args == []
 
 # /code, so eval it...
+
+# Try inspecting by default
 result = $el.eval $el.read "(progn #{args[0]})"
 
-return "<!" if ! result
+return if ! result
+
+result = $el.pp_to_string result
 
 Tree.quote result.to_s
