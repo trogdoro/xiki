8b4778e5c6035778aed02438d5b30747f0401bee
trogdoro <craig.muth@gmail.com>
2015-04-28 10:44:04 -0700

    Browser > use selenium to interact with chrome or firefox.

--- lib/xiki/tools/browser.rb
+++ lib/xiki/tools/browser.rb
@@ -1,21 +1,95 @@
 module Xiki
   class Browser
 
+    @@jquery_url = "http://code.jquery.com/jquery.min.js"
+    @@browser = {}
+
+    def self.browser
+
+      begin
+        require "selenium-webdriver"
+      rescue LoadError
+        return nil   # Callers shauld do something else if nil
+      end
+
+      kind = (Conf.get("browser", "browser") || "firefox").to_sym
+      begin
+        @@browser[kind] ||= Selenium::WebDriver.for kind
+        # Do something trivial, so it'll error if not connected
+        @@browser[kind].execute_script("return 1")
+      rescue Selenium::WebDriver::Error::WebDriverError=>e
+        # The old one was bad, so create entirely new connection
+        @@browser[kind] = Selenium::WebDriver.for kind
+      end
+
+      @@browser[kind]
+
+    end
+
     def self.url url, options={}
-      Firefox.url url, options
+
+      browser = self.browser
+
+      return Shell.command "open #{url}" if ! browser
+
+      # Firefox.url url, options
+      self.browser.navigate.to url
+
       "<! opened in browser"
     end
 
     def self.html html, options={}
-      Firefox.html html, options
+
+      name = options[:name] || "tmp"
+      File.open("/tmp/#{name}.html", "w") { |f| f << html }
+
+      self.url "file:///tmp/#{name}.html" #, :reload=>1
+
+      nil
     end
 
     def self.append html
+      raise "is this used somewhere?"
       Firefox.append html
     end
 
     def self.js txt, options={}
-      Firefox.exec txt, options
+
+      browser = self.browser
+
+      raise "
+        > A gem is required
+        | The selenium-webdriver gem must be installed to send js
+        | to the browser. To jump back to your shell and install
+        | it as a xiki-local gem, type ^G on this line:
+        |
+        $ xikigem install selenium-webdriver
+        " if ! browser
+
+      begin
+        browser.execute_script txt
+      rescue Exception=>e
+
+        # "$ is not defined" error, so load jquery and try again...
+
+        if e.message =~ /\$ is not defined/
+          browser.execute_script "
+            var s=document.createElement('script');
+            s.setAttribute('src', '#{@@jquery_url}'); s.setAttribute('id', 'jqid');
+            document.getElementsByTagName('head')[0].appendChild(s);
+
+            var s=document.createElement('script');
+            s.setAttribute('src', 'http://xiki.org/javascripts/util.js');
+            document.getElementsByTagName('head')[0].appendChild(s);
+            ".unindent
+
+          wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+          wait.until { browser.execute_script("return typeof( $ )") != "undefined" }
+
+          browser.execute_script txt
+        end
+      end
+
     end
 
     def self.open_in_browser
@@ -104,6 +178,7 @@ module Xiki
     end
 
     def self.reload
+      raise "is this used somewhere?"
       Firefox.reload
     end
 
