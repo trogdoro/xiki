d359d9eec749c4027518dbb8dc2af2a2b915fe36
trogdoro <craig.muth@gmail.com>
2016-08-24 16:18:44 -0700

    Bottom bar > improved.

--- lib/xiki/tools/themes.rb
+++ lib/xiki/tools/themes.rb
@@ -3,13 +3,13 @@ module Xiki
   # Lists themes and run them. Make files here
   # to make more themes:
   #
-  # =:xiki/misc/themes/
+  # =^xiki/misc/themes/
   #
   class Themes
 
     def self.use name # , options={}
 
-      path = "#{Xiki.dir}misc/themes/#{name.gsub " ", "_"}.notes"
+      path = "#{Xiki.dir}misc/themes/#{name.gsub " ", "_"}.xiki"
       raise "- Theme doesn't exist!" if ! File.exists? path
 
       load path
@@ -24,22 +24,22 @@ module Xiki
     def self.init_in_client
 
       $el.el4r_lisp_eval %`
-        (defun xiki-bar-add-padding (txt &optional space-face face)
+        (defun xiki-bar-add-padding (txt &optional space-face)
           (let ((total (window-width)) left right)
             (setq left (- total (length txt)))
             (setq left (/ left 2))
-            (when (> 0 left) (setq left 2))
+            (when (> 2 left) (setq left 2))
             (setq right (- total (+ left (length txt))))
-            (when (> 0 right) (setq right 2))
+            (when (> 2 right) (setq right 2))
             (if space-face
               (concat
                 (propertize (make-string left ? ) 'face space-face)   ; Make string of spaces
-                (if face (propertize txt 'face face) txt)
+                txt
                 (propertize (make-string right ? ) 'face space-face)   ; Make string of spaces
               )
               (concat
                 (make-string left ? )   ; Make string of spaces
-                (if face (propertize txt 'face face) txt)
+                txt
                 (make-string right ? )   ; Make string of spaces
               )
             )
